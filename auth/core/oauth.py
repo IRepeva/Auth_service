59b435a13380ec3b33576688fb8082cead83dd08
Irina Repeva <repeva.i@panoramik.ru>
2022-09-12 14:40:11 +0100

    Refactor oauth authorization

--- auth/core/oauth.py
+++ auth/core/oauth.py
@@ -1,12 +1,13 @@
+import abc
 from urllib.parse import urlencode
 
 import requests
-
 from api.models import User
 from api.v1.user_api import add_new_user
 from api.v1.utils.other import generate_fake_email
 from core.settings import oauth_settings
 from databases import db
+from api.v1.oauth_api import add_social_account
 
 OAUTH_DATA = {
     'yandex': {
@@ -23,17 +24,27 @@ OAUTH_DATA = {
 }
 
 
-class BaseOauthService:
+class BaseOauthProvider:
     SERVICE_NAME = None
     CLIENT_ID = None
     CLIENT_SECRET = None
+    PROVIDERS = None
 
     @property
-    def service_data(self):
+    def provider_data(self):
         return OAUTH_DATA[self.SERVICE_NAME]
 
+    @classmethod
+    def get_provider(cls, provider_name):
+        if cls.PROVIDERS is None:
+            cls.PROVIDERS = {}
+            for provider_class in cls.__subclasses__():
+                provider = provider_class()
+                cls.PROVIDERS[provider.SERVICE_NAME] = provider
+        return cls.PROVIDERS[provider_name]
+
     def make_authorize_url(self, **kwargs):
-        base_auth_url = self.service_data['authorization_endpoint'] + '?'
+        base_auth_url = self.provider_data['authorization_endpoint'] + '?'
         params = {'client_id': self.CLIENT_ID, 'response_type': 'code'}
         if kwargs:
             params.update(kwargs)
@@ -53,12 +64,14 @@ class BaseOauthService:
 
     def get_oauth_data(self, code, **kwargs):
         data = self.make_token_endpoint_data(code, **kwargs)
-        token_url = self.service_data['token_endpoint']
+        token_url = self.provider_data['token_endpoint']
         return requests.post(token_url, data=data).json()
 
-    def get_user(self, user_social_id, user_email=None):
-        from api.v1.oauth_api import add_social_account
+    @abc.abstractmethod
+    def get_user_data(self, user_info):
+        raise NotImplementedError('Method not implemented!')
 
+    def get_user(self, user_social_id, user_email=None):
         user = User.get_user_by_social_account(
             user_social_id, self.SERVICE_NAME
         )
@@ -79,13 +92,17 @@ class BaseOauthService:
         return user
 
 
-class YandexOAuth(BaseOauthService):
+class YandexOAuth(BaseOauthProvider):
     SERVICE_NAME = 'yandex'
     CLIENT_ID = oauth_settings.YANDEX_CLIENT_ID
     CLIENT_SECRET = oauth_settings.YANDEX_CLIENT_SECRET
 
+    def get_oauth_data(self, code, **kwargs):
+        tokens = super(YandexOAuth, self).get_oauth_data(code)
+        return self.get_user_info(tokens)
+
     def get_userinfo_url(self, **kwargs):
-        base_userinfo_url = self.service_data['userinfo_endpoint'] + '?'
+        base_userinfo_url = self.provider_data['userinfo_endpoint'] + '?'
         params = {'format': 'json', 'with_openid_identity': '1'}
         if kwargs:
             params.update(kwargs)
@@ -99,8 +116,20 @@ class YandexOAuth(BaseOauthService):
 
         return requests.post(user_info_request, headers=headers).json()
 
+    def get_user_data(self, user_info):
+        return user_info.get('id'), user_info['default_email']
 
-class VKOAuth(BaseOauthService):
+
+class VKOAuth(BaseOauthProvider):
     SERVICE_NAME = 'vk'
     CLIENT_ID = oauth_settings.VK_CLIENT_ID
     CLIENT_SECRET = oauth_settings.VK_CLIENT_SECRET
+
+    def make_authorize_url(self):
+        return super(VKOAuth, self).make_authorize_url(
+            redirect_uri=self.provider_data['redirect_uri'],
+            scope='email'
+        )
+
+    def get_user_data(self, user_info):
+        return user_info.get('user_id'), user_info.get('email')
